921b4b432013488df29785792bd063f1c34af846
MacoChave <totto_cha@hotmail.com>
2018-04-05 17:08:30 -0600

    Apariencia en ventanas

--- main.cpp
+++ main.cpp
@@ -4,6 +4,37 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString buttonStyle;
+    buttonStyle = "QPushButton {"
+                  "border-style: solid;"
+                  "border-width: 1px;"
+                  "border-color: rgb(255, 255, 255, 255);"
+                  "background-color: rgb(255, 255, 255, 180);"
+                  "color: rgb(255, 255, 255);"
+                  "border-radius: 6px;"
+                  "}";
+
+    QString frameStyle;
+    frameStyle = "QPushButton {"
+                 "border-style: solid;"
+                 "border-width: 1px;"
+                 "border-color: rgb(255, 255, 255, 180);"
+                 "background-color: rgb(255, 255, 255, 100);"
+                 "}";
+
+    QString windowStyle;
+    windowStyle = "QMainWindow {"
+                  "background-color: "
+                  "qlineargradient(spread:pad, "
+                  "x1:0.5, y1:1, x2:0.5, y2:0, "
+                  "stop:0.0700935 rgba(47, 41, 61, 255), "
+                  "stop:0.929907 rgba(0, 180, 156, 255));"
+                  "color: rgb(255, 255, 255);"
+                  "}";
+
+    qApp->setStyleSheet(buttonStyle + frameStyle + windowStyle);
+
     MainWindow w;
     w.setWindowTitle("Servidor");
     w.show();
